aca3a47510a2a4fc7b791a50222bdfc6ea55466d
Gary Coates <gary.coates@ipfin.co.uk>
2022-10-27 16:26:27 +0100

    Counter

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var peopleCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClickMe = findViewById<Button>(R.id.buttonClickMe)
+        buttonClickMe.setOnClickListener {
+            val textViewName = findViewById<TextView>(R.id.textViewName)
+            peopleCount++
+            textViewName.text = peopleCount.toString()
+            // Toast.makeText(this, "You counted $peopleCount times", Toast.LENGTH_LONG).show()
+        }
     }
 }
